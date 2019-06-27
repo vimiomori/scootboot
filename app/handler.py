fc7a68369f9e94a3945a266000016ef36fb129b5
vi <vivian.muchen@gmail.com>
2019-06-27 21:28:40 +0900

    add reply logic

--- app/handler.py
+++ app/handler.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import json
+import time
 
 
 END_POINT = "https://api.line.me/v2/bot/message/reply"
@@ -8,18 +9,38 @@ CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 class Handler:
 
     def __init__(self, event):
+        self.event = event
         self.reply_token = event["replyToken"]
         self.event_type = event["type"]
         self.user_id = event["source"]["userId"]
     
     def handle(self):
+        if self.event_type == "message":
+            self._reply(event["message"])
+        if self.event_type == "follow":
+            self._greet()
+
+    def _greet(self):
+        user_profile = self._get_profile(self.user_id)
+        display_name = user_profile["displayName"]
+        nick_name = self._nickname(display_name)
+        self._send("Hmm?")
+        time.sleep(2)
+        self._send(f"ooooOOOh! it's the {nick_name}!")
+        time.sleep(1)
+        self._send("What do you want?")
+
+    def _reply(self, msg):
+        reply = self._get_reply(msg)
+        self._send(reply)
+
+    def _send(self, msg):
         msg_body = json.dumps({
-            # "to": self.user_id,
             "replyToken": self.reply_token,
             "messages": [
                 {
                     "type":"text",
-                    "text":"hello"
+                    "text":msg
                 }
             ]
         })
@@ -33,4 +54,24 @@ class Handler:
             headers=headers,
             data=msg_body
         )
+    
+    def _get_profile(self, user_id):
+        url = f'https://api.line.me/v2/bot/profile/{user_id}'
+        header = {
+            "Authorization": "Bearer " + CHANNEL_ACCESS_TOKEN
+        }
+        res = requests.get(url, headers=header, timeout=(0.3, 1))
+        if res.status_code == 200:
+            return res.json()
+        else:
+            print(res)
+    
+    def _get_reply(self, msg):
+        if msg == "hello":
+            return "fuck off"
+    
+    def _nickname(self, display_name):
+        if display_name == "Garcia":
+            return "graaacie"
+
 
