fc8ea4e485d0e0104100a0becaa20388909c18cf
vi <vivian.muchen@gmail.com>
2019-06-29 16:50:58 +0900

    test

--- app/handler.py
+++ app/handler.py
@@ -23,14 +23,25 @@ class Handler:
             reply = self._get_reply("default")
         if self.event_type == "follow":
             reply = self._get_reply(self._get_profile(self.user_id))
-        self._send(reply)
+        self._reply(reply)
 
-    def _send(self, reply):
-        if type(reply) != list:
-            reply = [reply]
+    def _reply(self, reply):
+        if type(reply) == list:
+            r = None
+            for res in reply:
+                r = self._send(res)
+            return r
+        else:
+            return self._send(reply)
+
+    # def _reply(self, msg):
+    #     reply = self._get_reply(msg)
+    #     self._send(reply)
+
+    def _send(self, custom_res):
         msg_body = json.dumps({
             "replyToken": self.reply_token,
-            "messages": [self._construct_message(r) for r in reply]
+            "messages": [self._construct_message(custom_res)]
         })
         headers = {
             "Content-Type": "application/json",
@@ -68,7 +79,7 @@ class Handler:
     def _construct_message(self, res):
         print(res)
         message = {"type": res["type"]}
-        if res["type"] == "img":
+        if res["type"] == "image":
             message["originalContentUrl"] = res["response"]
         else:
             message["text"] = res["response"]
