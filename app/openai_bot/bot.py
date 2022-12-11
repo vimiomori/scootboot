ef9dab64ea3488765ff42620c48151917819addd
vimiomori <vivian.muchen@gmail.com>
2022-12-11 18:32:57 +0900

    feat: add model

--- app/openai_bot/bot.py
+++ app/openai_bot/bot.py
@@ -1,7 +1,8 @@
 import os
 import openai
 
-MODEL = "davinci:ft-personal-2022-12-11-08-21-53"
+MODEL1 = "davinci:ft-personal-2022-12-11-09-16-26" # trained without delimiters
+MODEL2 = "davinci:ft-personal-2022-12-11-08-21-53" # trained with delimiters
 
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -9,7 +10,7 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 remove_list = ["\n\n###\n\n", "\r", "\t", "END"]
 
 def get_response(prompt):
-    response = openai.Completion.create(model=MODEL, prompt=prompt, temperature=0.9, max_tokens=25)
+    response = openai.Completion.create(model=MODEL1, prompt=prompt, temperature=0.9, max_tokens=25)
     text = response["choices"][0]["text"]
     for remove in remove_list:
         text = text.replace(remove, "")
