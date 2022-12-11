e097d252e36e4ccb7af74c9b40afaa27df37cc39
vimiomori <vivian.muchen@gmail.com>
2022-12-11 18:34:25 +0900

    feat: remove more words

--- app/openai_bot/bot.py
+++ app/openai_bot/bot.py
@@ -7,7 +7,7 @@ MODEL2 = "davinci:ft-personal-2022-12-11-08-21-53" # trained with delimiters
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-remove_list = ["\n\n###\n\n", "\r", "\t", "END"]
+remove_list = ["\n\n###\n\n", "\r", "\t", "END OF LINE", "ENDING", "END"]
 
 def get_response(prompt):
     response = openai.Completion.create(model=MODEL1, prompt=prompt, temperature=0.9, max_tokens=25)
